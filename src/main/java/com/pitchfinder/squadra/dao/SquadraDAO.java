5507e860351dd0a00da9276a05efde73a4bd19d1
LuciaGGaeta <63348388+LuciaGGaeta@users.noreply.github.com>
2020-12-22 22:18:48 +0100

    Add methods doSaveSquadra and doRemoveSquadra in interface SquadraDAO

--- src/main/java/com/pitchfinder/squadra/dao/SquadraDAO.java
+++ src/main/java/com/pitchfinder/squadra/dao/SquadraDAO.java
@@ -1,4 +1,20 @@
 package com.pitchfinder.squadra.dao;
 
+
+import com.pitchfinder.squadra.entity.Squadra;
+
 public interface SquadraDAO {
+    /**
+     * If squadra is correctly SAVED in db return true else return false.
+     * @param squadra - Squadra that wants to be saved in the database.
+     * @return boolean
+     */
+    boolean doSaveSquadra(Squadra squadra);
+
+    /**
+     * If squadra is correctly REMOVED from database return true else return false.
+     * @param squadra - Squadra that wants to be removed from database
+     * @return boolean
+     */
+    boolean doRemoveSquadra(Squadra squadra);
 }
