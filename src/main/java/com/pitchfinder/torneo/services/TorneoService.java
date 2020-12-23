62485978db0c7d4e68b17390e18b75b911ed3114
pplomith <plomitallopaolo@gmail.com>
2020-12-23 19:04:09 +0100

    Add implementation TorneoServiceImpl Update interface TorneoService

--- src/main/java/com/pitchfinder/torneo/services/TorneoService.java
+++ src/main/java/com/pitchfinder/torneo/services/TorneoService.java
@@ -1,6 +1,7 @@
 package com.pitchfinder.torneo.services;
 
-import java.sql.Date;
+
+import java.util.Date;
 
 /**
  * This interface manages the TorneoService.
@@ -11,7 +12,6 @@ public interface TorneoService {
      * This method allows to create a tournament and invoke daSaveTorneo.
      * @param usernameAdmin admin username
      * @param idCampo pitch identifier
-     * @param sport sport of the tournament
      * @param nome name of the tournament
      * @param tipo type of the tournament
      * @param struttura structure of the tournament
@@ -23,6 +23,6 @@ public interface TorneoService {
      * @param giornoPartite match day of the tournament
      * @return boolean -> true : created / false : failed creation
      */
-    boolean createTorneo(String usernameAdmin, int idCampo, String sport, String nome, String tipo, String struttura, int maxSquadre,
+    boolean createTorneo(String usernameAdmin, int idCampo, String nome, String tipo, String struttura, int maxSquadre,
                          Date dataInizio, Date dateFine, int minPartecipanti, int maxPartecipanti, String giornoPartite);
 }
