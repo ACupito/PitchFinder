fc0e49f236472a8cff5f95b28cfd2e88ff13f7e5
LuciaGGaeta <63348388+LuciaGGaeta@users.noreply.github.com>
2020-12-23 08:47:14 +0100

    Add class PrenotaxioneDAOImpl Add class PrenotazioneDAO

---
+++ src/main/java/com/pitchfinder/prenotazione/dao/PrenotazioneDAOImpl.java
@@ -0,0 +1,62 @@
+package com.pitchfinder.prenotazione.dao;
+
+import com.pitchfinder.prenotazione.entity.Prenotazione;
+import com.pitchfinder.singleton.ConPool;
+
+import java.sql.*;
+
+public class PrenotazioneDAOImpl implements PrenotazioneDAO {
+
+    /**
+     * If prenotazione is correctly SAVED in database return true else return false.
+     * @param prenotazione - prenotazione.
+     * @return
+     */
+    @Override
+    public boolean doSavePrenotazione(Prenotazione prenotazione) {
+        try (Connection con = ConPool.getInstance().getConnection()) {
+            PreparedStatement ps =
+                    con.prepareStatement("INSERT INTO prenotazione(UtenteEmail, EventoNome, EventoData, CodicePrenotazione) VALUES (?, ?, ?, ?)");
+
+            ps.setString(1, prenotazione.getUtenteEmail());
+            ps.setString(2, prenotazione.getEventoNome());
+            ps.setDate(3, (Date) prenotazione.getEventoData());
+            ps.setInt(4, prenotazione.getCodicePrenotazione());
+
+            if (ps.executeUpdate() != 1) {
+                return false;
+            }
+
+            return true;
+
+        } catch (SQLException throwables) {
+            return false;
+        }
+    }
+
+    /**
+     * If prenotazione is correctly REMOVED from database return true else return false.
+     * @param prenotazione - prenotazione.
+     * @return
+     */
+    @Override
+    public boolean doRemovePrenotazione(Prenotazione prenotazione) {
+        try (Connection con = ConPool.getInstance().getConnection()) {
+            PreparedStatement ps =
+                    con.prepareStatement("DELETE FROM prenotazione WHERE CodicePrenotazione = ?");
+
+            ps.setInt(1, prenotazione.getCodicePrenotazione());
+
+            if (ps.executeUpdate() != 1) {
+                return false;
+            }
+
+            return true;
+
+        } catch (SQLException throwables) {
+            return false;
+        }
+    }
+
+
+}
