3e9bc6c8ed25989b4015c83df0005ef45f6e7b13
Paolo <paolo.db@users.noreply.github.com>
2020-12-23 10:26:10 +0100

    Add implementation of EventoDAO

--- src/main/java/com/pitchfinder/evento/dao/EventoDAO.java
+++ src/main/java/com/pitchfinder/evento/dao/EventoDAO.java
@@ -1,4 +1,45 @@
 package com.pitchfinder.evento.dao;
 
+import com.pitchfinder.evento.entity.Evento;
+
+import java.util.Date;
+import java.util.List;
+
 public interface EventoDAO {
+
+    /**
+     *
+     * @param event object
+     * @return boolean
+     */
+    boolean doSaveEvento(Evento event);
+
+    /**
+     *
+     * @param event object
+     * @return boolean
+     */
+    boolean doRemoveEvento(Evento event);
+
+    /**
+     *
+     * @return list<Evento>
+     */
+    List<Evento> doRetrieveByAllEventi();
+
+    /**
+     * The key of event are name, date.
+     * @param nome of the event
+     * @param date of the event
+     * @return Evento
+     */
+    Evento doRetrieveEvento(String nome, Date date);
+
+    /**
+     *
+     * @param event looking for its prenotation.
+     * @return List<Evento>
+     */
+    List<Evento> doRetrieveNPrenotazioniByEvento(Evento event);
+
 }
