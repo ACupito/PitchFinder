e6e54cc0f9f9f7a6c8b46ae5698a7bb838b77bf5
LuciaGGaeta <63348388+LuciaGGaeta@users.noreply.github.com>
2020-12-24 15:22:00 +0100

    Add initial PrenotazioneServiceImplTest

---
+++ src/test/java/service/PrenotazioneServiceImplTest.java
@@ -0,0 +1,18 @@
+package service;
+
+import com.pitchfinder.evento.entity.Evento;
+import com.pitchfinder.prenotazione.entity.Prenotazione;
+import org.junit.Test;
+
+import java.util.Date;
+
+public class PrenotazioneServiceImplTest {
+
+    @Test
+    void checkCreatePrenotazione(){
+        Date data = new Date();
+        Evento evento = new Evento("Evento di Prova 11","web/evento/immagini/eventoImage1","04:00:00","Giuseppe Verdi",2000-10-03,"Giuseppe Verdi","Questo è un evento fittizio di prova",150,"memex99");
+
+        Prenotazione prenotazione = new Prenotazione("lucia.gaeta.98@gmail.com", );
+    }
+}
