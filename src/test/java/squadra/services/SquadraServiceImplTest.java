9470659f3792d8fcf1d220511acf91e76b077473
LuciaGGaeta <63348388+LuciaGGaeta@users.noreply.github.com>
2020-12-24 09:32:08 +0100

    Add Method createSquadra

---
+++ src/test/java/squadra/services/SquadraServiceImplTest.java
@@ -0,0 +1,18 @@
+package squadra.services;
+
+import com.pitchfinder.squadra.services.SquadraService;
+import com.pitchfinder.squadra.services.SquadraServiceImpl;
+import com.pitchfinder.torneo.entity.Torneo;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+public class SquadraServiceImplTest {
+    SquadraService squadraService = new SquadraServiceImpl();
+
+    @Test
+    public void checkCreateSquadra(){
+
+    }
+}
