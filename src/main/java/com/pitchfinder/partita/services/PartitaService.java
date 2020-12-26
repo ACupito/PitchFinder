e193ea5736f04c87ade07cc7fa225c6001c25529
eugenio12simone <eugenio12simone@gmail.com>
2020-12-26 11:29:09 +0100

    Add PartitaService Add PartitaServiceImpl Add PartitaServiceImplTest Fix import in PartitaDAOImplTest

--- src/main/java/com/pitchfinder/partita/services/PartitaService.java
+++ src/main/java/com/pitchfinder/partita/services/PartitaService.java
@@ -1,4 +1,20 @@
 package com.pitchfinder.partita.services;
 
+import com.pitchfinder.autenticazione.entity.Utente;
+import com.pitchfinder.partita.entity.Partita;
+
+import java.sql.Date;
+import java.sql.Time;
+
 public interface PartitaService {
+    /**
+     *  This method is used by the user during a match booking.
+     * @param idCampo idCampo
+     * @param utente utente
+     * @param date date
+     * @param start start
+     * @param end end
+     * @return Partita
+     */
+    Partita createPartita(int idCampo, Utente utente, Date date, Time start, Time end);
 }
