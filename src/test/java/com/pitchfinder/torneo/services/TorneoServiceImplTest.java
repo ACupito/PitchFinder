6d680c2f1198a66ae501d51eda8e6d1e3891a3f4
pplomith <plomitallopaolo@gmail.com>
2020-12-26 17:31:34 +0100

    TorneoServiceImplTest

--- src/test/java/com/pitchfinder/torneo/services/TorneoServiceImplTest.java
+++ src/test/java/com/pitchfinder/torneo/services/TorneoServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.pitchfinder.torneo.services;
 
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -29,10 +28,35 @@ class TorneoServiceImplTest {
     }
 
     /**
-     * This method tests the method createTorneo.
+     * This method tests the method createTorneo in case it fails.
      */
     @Test
-    void createTorneoTest() {
+    void checkCreateTorneoTest_1() {
+
+        String usernameAdmin = "";
+        String nome = "Champions";
+        int idCampo = 0;
+        String tipo = "Gironi";
+        String struttura = "Partite singole";
+        int maxSquadre = 12;
+        int minPartecipanti = 1;
+        int maxPartecipanti = 2;
+        String giornoPartite = "Lunedì";
+        Date dataInizio = Date.valueOf("2020-12-24");
+        Date dataFine = Date.valueOf("2020-12-31");
+
+        //check if the method return true
+        assertFalse(tservice.createTorneo(usernameAdmin, idCampo, nome, tipo,
+                struttura, maxSquadre, dataInizio, dataFine, minPartecipanti, maxPartecipanti,
+                giornoPartite));
+
+    }
+
+    /**
+     * This method tests the method createTorneo in case it successful.
+     */
+    @Test
+    void checkCreateTorneoTest_2() {
 
         String usernameAdmin = "memex99";
         String nome = "Champions";
@@ -43,13 +67,41 @@ class TorneoServiceImplTest {
         int minPartecipanti = 1;
         int maxPartecipanti = 2;
         String giornoPartite = "Lunedì";
-        Date dataInizio = new Date(2020 - 1900, 11, 24);
-        Date dataFine = new Date(2020 - 1900, 11, 31);
+        Date dataInizio = Date.valueOf("2020-12-24");
+        Date dataFine = Date.valueOf("2020-12-31");
 
         //check if the method return true
         assertTrue(tservice.createTorneo(usernameAdmin, idCampo, nome, tipo,
                 struttura, maxSquadre, dataInizio, dataFine, minPartecipanti, maxPartecipanti,
                 giornoPartite));
+
+    }
+
+    /**
+     * This method tests the method deleteTorneo in case it fails.
+     */
+    @Test
+    void checkDeleteTorneoTest_3() {
+
+        int idCampo = 0;
+        String nome = "";
+        Date dataInizio = Date.valueOf("2020-12-24");
+
+        assertFalse(tservice.deleteTorneo(idCampo, nome, dataInizio));
+
+    }
+
+    /**
+     * This method tests the method deleteTorneo in case it successful.
+     */
+    @Test
+    void checkDeleteTorneoTest_4() {
+
+        int idCampo = 1002;
+        String nome = "Champions";
+        Date dataInizio = Date.valueOf("2020-12-24");
+
+        assertTrue(tservice.deleteTorneo(idCampo, nome, dataInizio));
 
     }
 
