22b771bfe303786474466deda2089952a7d33f76
eugenio12simone <eugenio12simone@gmail.com>
2020-12-26 10:14:33 +0100

    Update checkdoSavePartita & Update checkdoRetrieveAll Fix doRetrieveAll in PartitaDAOImpl

--- src/test/java/partita/dao/PartitaDAOImplTest.java
+++ src/test/java/partita/dao/PartitaDAOImplTest.java
@@ -8,13 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.util.List;
 
 public class PartitaDAOImplTest {
     /**
-     * This method check correct behaviour of doSavePartita
+     * This method check correct behaviour of doSavePartita.
      */
     @Test
-    public void checkdoSavePartitaTr() {
+    public void checkdoSavePartita() {
         Date date = new Date(2020-1900,11,28);
         Time start = new Time(16,00,00);
         Time end = new Time(18,00,00);
@@ -26,20 +27,15 @@ public class PartitaDAOImplTest {
 
         assertTrue(dao.doSavePartita(pTest));
     }
+
     /**
-     * This method check bad behaviour of doSavePartita
+     * This method check correct behaviour of doRetrieveAll.
      */
     @Test
-    public void checkdoSavePartitaFl() {
-        Date date = new Date(2020-1900,11,28);
-        Time start = new Time(16,00,00);
-        Time end = new Time(18,00,00);
-
-        Partita pTest = new Partita(9999,"mario96@gmail.com",
-                date,start,end);
-
+    public void checkdoRetrieveAll() {
         PartitaDAO dao = new PartitaDAOImpl();
+        List<Partita> partite = dao.doRetrieveAll();
 
-        assertThrows(RuntimeException.class ,() -> {dao.doSavePartita(pTest);});
+        assertNotNull(partite);
     }
 }
