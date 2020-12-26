f8de14a4a405c499973c8a0e06ca8a979db99ff4
eugenio12simone <eugenio12simone@gmail.com>
2020-12-26 18:45:12 +0100

    Add implementation for the method doSaveGiocatore Add implementation for the method doRetrieveAllGiocatori Update PartitaDAOImplTest

--- src/test/java/com/pitchfinder/partita/dao/PartitaDAOImplTest.java
+++ src/test/java/com/pitchfinder/partita/dao/PartitaDAOImplTest.java
@@ -1,14 +1,44 @@
 package com.pitchfinder.partita.dao;
 
+import com.pitchfinder.autenticazione.dao.UtenteDAO;
+import com.pitchfinder.autenticazione.dao.UtenteDAOImpl;
+import com.pitchfinder.autenticazione.entity.Utente;
 import com.pitchfinder.partita.entity.Partita;
+import com.pitchfinder.singleton.ConPool;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.sql.Date;
-import java.sql.Time;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PartitaDAOImplTest {
+    private PartitaDAO daoTest;
+
+    @BeforeAll
+    public void start(){
+        daoTest = new PartitaDAOImpl();
+
+        UtenteDAO daoUtente = new UtenteDAOImpl();
+        Utente userTest = new Utente();
+        userTest.setEmail("test99@gmail.com");
+        userTest.setUsername("Testing");
+        userTest.setNome("Test");
+        userTest.setCognome("De Test");
+        userTest.setPassword("Testing99@");
+
+        Date dateOfBirth = new Date(1999-1900,11,28);
+        userTest.setDataDiNascita(dateOfBirth);
+
+        daoTest.doSaveGiocatore(4,"Pasquale","Gaeta");
+        daoUtente.doSaveUtente(userTest);
+    }
+
     /**
      * This method check correct behaviour of doSavePartita.
      */
@@ -18,10 +48,8 @@ public class PartitaDAOImplTest {
         Time start = new Time(16,00,00);
         Time end = new Time(18,00,00);
 
-        Partita pTest = new Partita(1002,"mario96@gmail.com",
+        Partita pTest = new Partita(1002,"test99@gmail.com",
                 date,start,end);
-
-        PartitaDAO daoTest = new PartitaDAOImpl();
 
         assertTrue(daoTest.doSavePartita(pTest));
     }
@@ -31,9 +59,72 @@ public class PartitaDAOImplTest {
      */
     @Test
     public void checkdoRetrieveAll() {
-        PartitaDAO daoTest = new PartitaDAOImpl();
         List<Partita> partite = daoTest.doRetrieveAll();
 
         assertNotNull(partite);
     }
+
+    /**
+     * This method check correct behaviour of doSaveGiocatore.
+     */
+    @Test
+    public void checkdoSaveGiocatore(){
+
+        assertTrue(daoTest.doSaveGiocatore(4,"Andrea","Rossi"));
+    }
+
+    /**
+     * This method check correct behaviour of doRetrieveAllGiocatori
+     */
+    @Test
+    public void doRetrieveAllGiocatori(){
+        List<String> giocatori = daoTest.doRetrieveAllGiocatori(4);
+
+        assertNotNull(giocatori);
+    }
+
+    @AfterAll
+    public void clean(){
+        //Delete player used for the testing
+
+        try (Connection con = ConPool.getInstance().getConnection()) {
+
+            String query = "DELETE FROM giocatore "
+                    + "WHERE PartitaIdentificativoPartita = 4";
+            PreparedStatement ps =
+                    con.prepareStatement(query);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Delete user used for the testing
+        try (Connection con = ConPool.getInstance().getConnection()) {
+
+            String query = "DELETE FROM utente "
+                    + "WHERE Email = 'test99@gmail.com' ";
+            PreparedStatement ps =
+                    con.prepareStatement(query);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Delete user used for the testing
+        try (Connection con = ConPool.getInstance().getConnection()) {
+
+            String query = "DELETE FROM partita "
+                    + "WHERE UtenteEmail = 'test99@gmail.com' ";
+            PreparedStatement ps =
+                    con.prepareStatement(query);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
