d1e99b623d7ecef5e97bc41a3c366fc6acd22fef
Paolodbuono <p.dellobuono@studenti.unisa.it>
2020-12-27 21:58:38 +0100

    Revert "Delete all package beside Evento and Autenticazione"
    This reverts commit beca2f78

---
+++ src/main/java/com/pitchfinder/campo/services/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the classes which manage CampoService and CampoServiceImpl.
+ */
+
+package com.pitchfinder.campo.services;
---
+++ src/main/java/com/pitchfinder/partita/dao/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package manages all the features of the partita dao.
+ */
+
+package com.pitchfinder.partita.dao;
