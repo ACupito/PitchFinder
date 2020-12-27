f5bdd4be7f3982a5697642e63d4d80422d046ec8
EDesimone12 <32223916+EDesimone12@users.noreply.github.com>
2020-12-27 20:30:19 +0100

    Revert "Feature/registration"

--- src/main/java/com/pitchfinder/autenticazione/dao/UtenteDAO.java
+++ src/main/java/com/pitchfinder/autenticazione/dao/UtenteDAO.java
@@ -2,7 +2,6 @@ package com.pitchfinder.autenticazione.dao;
 
 import com.pitchfinder.autenticazione.entity.Utente;
 
-
 public interface UtenteDAO {
 
     /**
@@ -19,11 +18,4 @@ public interface UtenteDAO {
      * @return boolean
      */
     Utente checkUtente(Utente utente);
-
-    /**
-     * This method returns a list of users using input emails.
-     * @param u is the user
-     * @return utente
-     */
-    Utente doRetrieveUtenteByEmail(Utente u);
 }
