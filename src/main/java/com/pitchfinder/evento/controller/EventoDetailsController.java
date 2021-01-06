d8fa83f947ffec0e14ac0682c6f9d22c809dee83
Paolo <p.dellobuono@studenti.unisa.it>
2021-01-06 01:20:10 +0100

    Add eventoDetailsController

---
+++ src/main/java/com/pitchfinder/evento/controller/EventoDetailsController.java
@@ -0,0 +1,41 @@
+package com.pitchfinder.evento.controller;
+
+import com.pitchfinder.evento.entity.Evento;
+import com.pitchfinder.evento.services.EventoService;
+import com.pitchfinder.evento.services.EventoServiceImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/EventoDetailsController")
+public class EventoDetailsController extends HttpServlet {
+
+    /**
+     * doPost() method.
+     * @param request is the servlet request.
+     * @param response is the servlet response.
+     */
+    public void doPost(final HttpServletRequest request,
+                       final HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    /**
+     * doGet() method.
+     * @param request is the servlet request.
+     * @param response is the servlet response.
+     */
+    public void doGet(final HttpServletRequest request,
+                      final HttpServletResponse response) throws ServletException, IOException {
+
+        RequestDispatcher dispatcher =
+                request.getRequestDispatcher("eventoDetails.jsp");
+        dispatcher.forward(request, response);
+    }
+}
