1066a8f9f313fc3a034161c1aaa127a4debc6c85
Paolo <p.dellobuono@studenti.unisa.it>
2021-01-12 13:28:01 +0100

    Fix evento getStringDay Method

--- src/main/java/com/pitchfinder/evento/entity/Evento.java
+++ src/main/java/com/pitchfinder/evento/entity/Evento.java
@@ -142,19 +142,19 @@ public class Evento {
      */
     public String getStringDay(int day) {
         switch (day) {
-            case 0:
-                return "Lun";
             case 1:
-                return "Mar";
+                return "Lun";
             case 2:
-                return "Mer";
+                return "Mar";
             case 3:
-                return "Gio";
+                return "Mer";
             case 4:
-                return "Ven";
+                return "Gio";
             case 5:
-                return "Sab";
+                return "Ven";
             case 6:
+                return "Sab";
+            case 7:
                 return "Dom";
             default:
                 return "";
