165cd123244365a6a6a74a18343a8c62f72daac7
Paolo <p.dellobuono@studenti.unisa.it>
2021-01-12 12:39:09 +0100

    Update Evento.java (Entity)

--- src/main/java/com/pitchfinder/evento/entity/Evento.java
+++ src/main/java/com/pitchfinder/evento/entity/Evento.java
@@ -100,6 +100,42 @@ public class Evento {
     }
 
     /**
+     * Used for the event.jsp.
+     * @param month
+     * @return String
+     */
+    public String getStringMonth(int month) {
+        switch (month) {
+            case 1:
+                return "Gen";
+            case 2:
+                return "Feb";
+            case 3:
+                return "Mar";
+            case 4:
+                return "Apr";
+            case 5:
+                return "Mag";
+            case 6:
+                return "Giu";
+            case 7:
+                return "Lug";
+            case 8:
+                return "Ago";
+            case 9:
+                return "Set";
+            case 10:
+                return "Ott";
+            case 11:
+                return "Nov";
+            case 12:
+                return "Dic";
+            default:
+                return "";
+        }
+    }
+
+    /**
      * @return String
      */
     public String getGuest() {
