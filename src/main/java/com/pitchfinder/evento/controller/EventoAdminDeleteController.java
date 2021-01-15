01cfb2d45437a7f673f1e8fb0452748ece6e6feb
Paolo <p.dellobuono@studenti.unisa.it>
2021-01-15 10:41:19 +0100

    Add EventoAdminDeleteController and EventoAdminDeleteControllerTest

---
+++ src/main/java/com/pitchfinder/evento/controller/EventoAdminDeleteController.java
@@ -0,0 +1,100 @@
+package com.pitchfinder.evento.controller;
+
+import com.pitchfinder.autenticazione.entity.Admin;
+import com.pitchfinder.evento.entity.Evento;
+import com.pitchfinder.evento.services.EventoService;
+import com.pitchfinder.evento.services.EventoServiceImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+
+@WebServlet("/EventoAdminController")
+public class EventoAdminDeleteController extends HttpServlet {
+
+    /** Minimum limit for name. */
+    private static final int MINLIMIT = 1;
+    /** Maximum limit for name. */
+    private static final int MAXLIMIT = 50;
+    /** Maximum limit for image. */
+    private static final int MAXLIMITIMAGE = 2;
+    /** Maximum limit for the Guest. */
+    private static final int MAXGUESTLIMIT = 20;
+    /** Maximum limit for the description. */
+    private static final int MAXDESCRIPTIONLIMIT = 500;
+    /** Maximum limit for the description. */
+    private static final int MAXSITSLIMIT = 300;
+
+    /**
+     * doPost() method.
+     * @param request is the servlet request.
+     * @param response is the servlet response.
+     */
+    public void doPost(final HttpServletRequest request,
+                       final HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    /**
+     * doGet() method.
+     * @param request is the servlet request.
+     * @param response is the servlet response.
+     */
+    public void doGet(final HttpServletRequest request,
+                      final HttpServletResponse response) throws ServletException, IOException {
+
+        Admin admin = (Admin) request.getSession().getAttribute("admin"); //get admin from the session
+        RequestDispatcher dispatcher;
+
+        if (admin != null) {
+            /* The EventoService object. */
+            EventoService es = new EventoServiceImpl();
+            /* The name of the Event (String). */
+            String nome = request.getParameter("nome");
+            /* The Event's date (String). */
+            String dateStr = request.getParameter("data");
+            /* The Event's date (Date)*/
+            Date dataEvento;
+
+            if (nome.length() < MINLIMIT || nome.length() > MAXLIMIT) {
+                throw new IllegalArgumentException("Errato: lunghezza nome non valida");
+            }
+            if (!nome.matches("^[ a-zA-Z\u00C0-\u00ff']+$")) {
+                throw new IllegalArgumentException("Errato: formato non valido");
+            }
+
+            if (dateStr.equals("")) {
+                throw new IllegalArgumentException("Errato: data non selezionata");
+            }
+            Date myDate = new Date(System.currentTimeMillis());
+            try {
+                dataEvento = Date.valueOf(dateStr); /* The Event's date (Date). */
+                if (dataEvento.before(myDate)) {
+                    throw new IllegalArgumentException("Errato: formato non valido");
+                }
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("Errato: formato non valido");
+            }
+
+            Evento temp = new Evento(nome, "default", new Time(12, 00, 00),
+                    new Time(14, 00, 00), dataEvento, "temp", "temp",
+                    0, admin.getUsername());
+            if (temp == null) {
+                response.setContentType("Impossibile eliminare l'evento");
+            }
+
+            response.setContentType("Eliminazione avvenuta");
+            dispatcher = request.getServletContext().getRequestDispatcher("/view/autenticazione/admin.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
+
+
+}
---
+++ src/test/java/com/pitchfinder/evento/controller/EventoAdminDeleteControllerTest.java
@@ -0,0 +1,216 @@
+package com.pitchfinder.evento.controller;
+
+import com.pitchfinder.autenticazione.entity.Admin;
+import com.pitchfinder.evento.entity.Evento;
+import com.pitchfinder.evento.services.EventoService;
+import com.pitchfinder.evento.services.EventoServiceImpl;
+import com.pitchfinder.singleton.ConPool;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mockito;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class EventoAdminDeleteControllerTest {
+
+    private EventoAdminDeleteController servlet;
+    private HttpServletRequest mockedRequest;
+    private HttpServletResponse mockedResponse;
+    private ServletContext mockedServletContext;
+    private RequestDispatcher mockedDispatcher;
+    private HttpSession session;
+    private Admin admin = new Admin();
+
+    /**
+     * Parameters declaration.
+     */
+    private static final String NOME = "EventoTennis";
+    private static final String IMMAGINE = "default";
+    private static final String ORARIO_INIZIO = "10:00";
+    private static final String ORARIO_FINE = "12:00";
+    private static final String DATA = "2021-12-31";
+    private static final String OSPITE = "Mariani Fittipaldi";
+    private static final String DESCRIZIONE = "Evento per aggiornare gli interessati su nuove regole arbitrali";
+    private static final String POSTI_DISPONIBILI = "150";
+
+    /**
+     *  Setting up the enviroment.
+     */
+    @BeforeAll
+    void setUp() {
+
+        //Servlet, mockedRequest, mockedResponse and Session instantiation.
+        servlet = new EventoAdminDeleteController();
+        mockedRequest = Mockito.mock(HttpServletRequest.class);
+        mockedResponse = Mockito.mock(HttpServletResponse.class);
+        mockedServletContext = Mockito.mock(ServletContext.class);
+        mockedDispatcher = Mockito.mock(RequestDispatcher.class);
+        session = Mockito.mock(HttpSession.class);
+
+
+
+        //Admin creation for the session.
+        admin.setNome("Paolo");
+        admin.setCognome("DB");
+        admin.setUsername("testAdmin05");
+        admin.setPassword("password");
+
+        try (Connection con = ConPool.getInstance().getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "INSERT INTO Admin (Nome, Cognome, Username, Password) VALUES(?,?,?,?)");
+            ps.setString(1, admin.getNome());
+            ps.setString(2, admin.getCognome());
+            ps.setString(3, admin.getUsername());
+            ps.setString(4, admin.getPasswordHash());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        //session setting.
+        Mockito.when(mockedRequest.getSession()).thenReturn(session);
+        Mockito.when(mockedRequest.getSession().getAttribute("admin")).thenReturn(admin);
+
+    }
+
+    /**
+     * Evento's name is empity.
+     */
+    @Test
+    void TC_11_16() {
+
+        Mockito.when(mockedRequest.getParameter("nome")).thenReturn("");
+        Mockito.when(mockedRequest.getParameter("data")).thenReturn(DATA);
+
+        String message = "Errato: lunghezza nome non valida";
+
+        IllegalArgumentException exception;
+        exception = assertThrows(IllegalArgumentException.class,
+                () -> servlet.doGet(mockedRequest, mockedResponse));
+        assertEquals(message, exception.getMessage());
+
+    }
+
+    /**
+     * Evento's name is not valid.
+     */
+    @Test
+    void TC_11_17() {
+
+        Mockito.when(mockedRequest.getParameter("nome")).thenReturn("Evento%%&&&£");
+        Mockito.when(mockedRequest.getParameter("data")).thenReturn(DATA);
+
+        String message = "Errato: formato non valido";
+
+        IllegalArgumentException exception;
+        exception = assertThrows(IllegalArgumentException.class,
+                () -> servlet.doGet(mockedRequest, mockedResponse));
+        assertEquals(message, exception.getMessage());
+
+    }
+
+    /**
+     * Evento's data is not empity.
+     */
+    @Test
+    void TC_11_18(){
+        Mockito.when(mockedRequest.getParameter("nome")).thenReturn(NOME);
+        Mockito.when(mockedRequest.getParameter("data")).thenReturn("");
+
+        String message = "Errato: data non selezionata";
+
+        IllegalArgumentException exception;
+        exception = assertThrows(IllegalArgumentException.class,
+                () -> servlet.doGet(mockedRequest, mockedResponse));
+        assertEquals(message, exception.getMessage());
+    }
+
+    /**
+     * Evento's data is not valid.
+     */
+    @Test
+    void TC_11_19(){
+        Mockito.when(mockedRequest.getParameter("nome")).thenReturn(NOME);
+        Mockito.when(mockedRequest.getParameter("data")).thenReturn("2019-12-22");
+
+        String message = "Errato: formato non valido";
+
+        IllegalArgumentException exception;
+        exception = assertThrows(IllegalArgumentException.class,
+                () -> servlet.doGet(mockedRequest, mockedResponse));
+        assertEquals(message, exception.getMessage());
+    }
+
+    /**
+     * Everything is working fine! Good job.
+     */
+    @Test
+    void TC_11_20() throws ServletException, IOException {
+        Mockito.when(mockedRequest.getParameter("nome")).thenReturn(NOME);
+        Mockito.when(mockedRequest.getParameter("data")).thenReturn(DATA);
+
+        Mockito.doReturn(mockedServletContext).when(mockedRequest).getServletContext();
+        Mockito.doReturn(mockedDispatcher).when(mockedServletContext).getRequestDispatcher("/view/autenticazione/admin.jsp");
+
+        servlet.doPost(mockedRequest, mockedResponse);
+        Mockito.verify(mockedResponse).setContentType("Eliminazione avvenuta");
+
+
+    }
+
+    /**
+     * Cleanup the environment.
+     */
+    @AfterAll
+    void tearDown() {
+
+        servlet = null;
+        mockedRequest = null;
+        mockedResponse = null;
+        mockedServletContext = null;
+        mockedDispatcher = null;
+        session = null;
+        EventoService es = new EventoServiceImpl();
+        Evento evento = new Evento();
+        evento.setName(NOME);
+        evento.setImage(IMMAGINE);
+        evento.setDate(Date.valueOf(DATA));
+        evento.setStartHour(Time.valueOf(ORARIO_INIZIO.concat(":00")));
+        evento.setEndHour(Time.valueOf(ORARIO_FINE.concat(":00")));
+        evento.setGuest(OSPITE);
+        evento.setDescription(DESCRIZIONE);
+        evento.setAvailableSits(Integer.parseInt(POSTI_DISPONIBILI));
+        evento.setAdmin(admin.getUsername());
+        es.removeEvento(evento);
+
+        admin.setNome("Paolo");
+        admin.setCognome("DB");
+        admin.setUsername("testAdmin05");
+        admin.setPassword("password");
+
+        try (Connection con = ConPool.getInstance().getConnection()) {
+            PreparedStatement ps =
+                    con.prepareStatement("delete from Admin where username=? && password=?");
+
+            ps.setString(1, admin.getUsername());
+            ps.setString(2, admin.getPasswordHash());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
