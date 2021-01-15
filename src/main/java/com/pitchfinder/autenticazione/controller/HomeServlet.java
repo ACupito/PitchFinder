50e99c64f0168432afa50334405929ab14a42db7
Paolo <p.dellobuono@studenti.unisa.it>
2021-01-15 12:11:24 +0100

    Fix all CheckStyleViolations

--- src/main/java/com/pitchfinder/autenticazione/controller/HomeServlet.java
+++ src/main/java/com/pitchfinder/autenticazione/controller/HomeServlet.java
@@ -11,6 +11,13 @@ import java.io.IOException;
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
 
+    /**
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
