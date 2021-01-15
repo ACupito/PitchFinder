4c051a4a367400059dc89a580350a3273b9d6711
Paolo <p.dellobuono@studenti.unisa.it>
2021-01-15 18:20:08 +0100

    Add Javscript for Event Creation

--- src/main/webapp/js/autenticazione/js_admin.js
+++ src/main/webapp/js/autenticazione/js_admin.js
@@ -66,4 +66,75 @@
 });
 });
 
+/**** TOURNAMENT Javascript *****/
 
+/**** END TOURNAMENT Javascript *****/
+
+/**** EVENT Javascript *****/
+var isDateValidEvento=false;
+var isTimeValid=false;
+var isNameValid=false;
+var isSurnameValid=false;
+var isPlayerNumberValid=false;
+var numberOfCheckedItems = 0;
+var nPlayer=0;
+var hhStrEvento;
+var mmStrEvento;
+var hhEndEvento;
+var mmEndEvento;
+
+    /** Name Evento **/
+    //fill
+
+
+    /** Date Evento **/
+    /** min Date **/
+    function minDateEvento() {
+        var currentDate = new Date();
+        var dd = String(currentDate.getDate()).padStart(2, '0');
+        var mm = String(currentDate.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = currentDate.getFullYear();
+
+        currentDate = yyyy + '-' + mm + '-' + dd;
+        $('#creation-data-Evento').attr("min",currentDate);
+    }
+    /** max Date **/
+    function maxDateEvento(){
+        var currentDate = new Date();
+        var dd = String(currentDate.getDate()).padStart(2, '0');
+        var mm = String(currentDate.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = currentDate.getFullYear();
+        currentDate = (yyyy+10) + '-' + mm + '-' + dd;
+
+        $('#creation-data-Evento').attr("max",currentDate);
+    }
+    /** valiDateEvento **/
+    function valiDateEvento(){
+        if(document.getElementById("creation-data-Evento").value.match("^[0-9]{4}\\-[0-9]{2}\\-[0-9]{2}$")){
+            $("#creation-label-data-Evento").css("color","#4CAF50");
+            $("#date-evento-valid").text("La data è valida, rispetta il formato");
+            $("#date-evento-valid").css("color","#4CAF50");
+            isDateValidEvento=true;
+        }else{
+            $("#creation-label-data-Evento").css("color","#FF0000");
+            $("#date-evento-valid").text("La data non rispetta il formato");
+            $("#date-evento-valid").css("color","#FF0000");
+            isDateValidEvento=false;
+        }
+    }
+
+function validateEvento(){
+
+}
+
+    $(document).ready(function(){
+        minDateEvento();
+        maxDateEvento();
+    });
+
+/**** END EVENT Javascript *****/
+
+/**** MATCH Javascript *****/
+
+
+/**** END MATCH Javascript *****/
