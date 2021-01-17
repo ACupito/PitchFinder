5a526c2a73d10fb51b23022348652b1fcf394283
Paolo <p.dellobuono@studenti.unisa.it>
2021-01-17 20:31:53 +0100

    Fix checkstyle violations

--- src/main/java/com/pitchfinder/evento/controller/EventoAdminController.java
+++ src/main/java/com/pitchfinder/evento/controller/EventoAdminController.java
@@ -152,7 +152,8 @@ public class EventoAdminController extends HttpServlet {
             }
 
             response.setContentType("La creazione dell’evento è andata a buon fine");
-            RequestDispatcher requestDispatcher = request.getServletContext().getRequestDispatcher("/autentication?flag=5&result=1&message=La creazione dell’evento è andata a buon fine");
+            RequestDispatcher requestDispatcher = request.getServletContext().getRequestDispatcher("/autentication?"
+                    + "flag=5&result=1&message=La creazione dell’evento è andata a buon fine");
             requestDispatcher.forward(request, response);
 
         }
