0d993f3d580d7c2d068c92970c61b20937c3c1fa
LuciaGGaeta <63348388+LuciaGGaeta@users.noreply.github.com>
2021-01-19 09:37:18 +0100

    add Iscrizionetorneo package. add TC_1_2_1 add TC_1_2_2 add TC_1_2_3 add TC_1_2_4 add TC_1_2_5 add TC_1_2_6 add TC_1_2_7 add TC_1_2_8 add TC_1_2_9 add TC_1_2_10 add TC_1_2_11

--- src/test/java/systemTesting/gestioneTornei/IscrizioneTorneo/TC_2_2_11.java
+++ src/test/java/systemTesting/gestioneTornei/IscrizioneTorneo/TC_2_2_11.java
@@ -1,4 +1,98 @@
 package systemTesting.gestioneTornei.IscrizioneTorneo;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TC_2_2_11 {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @AfterAll
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    @BeforeAll
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "src/test/java/systemTesting/katalonDriver/chromedriver.exe");
+        driver = new ChromeDriver();
+        baseUrl = "https://www.google.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+
+    }
+
+    @Test
+    public void TC311() throws Exception {
+
+        driver.get("http://localhost:8080/PitchFinder_war_exploded/");
+        driver.findElement(By.id("dropdownMenu1")).click();
+        driver.findElement(By.id("username")).click();
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys("Meglio100");
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("Esse3");
+        driver.findElement(By.id("login")).click();
+
+
+
+        driver.get("http://localhost:8080/PitchFinder_war_exploded/IscrizioneTorneoController?nomeTorneo=Champions+Five&dataTorneo=2021-01-18&campoTorneo=1002&nomeSquadra=I magnifici&minP=4&maxP=6&nGiocatori=4&&nome0=marco&nome1=Antonia&nome2=franco&nome3=paolo&cognome0=giglio&cognome1=bruno&cognome2=gaeta&cognome3=buono&nomeCapitano=Lucia&cognomeCapitano=Gaeta&nomeTorneo=Champions+Five&campo=1002&dataTorneo=2021-01-18&conferma=conferma");
+
+        assertEquals("La disponibilità viene memorizzata con successo.", closeAlertAndGetItsText());
+    }
+
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, 15);
+            Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+
 }
